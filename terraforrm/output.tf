dae5f27caff69a25912567debcaa5880977326eb
Prashant dubey <pacific.prashantdubey@gmail.com>
2025-07-07 13:36:40 +0530

    Delete terraforrm directory

--- terraforrm/output.tf
+++
@@ -1,4 +0,0 @@
-output "webhook_url" {
-  value       = "${aws_api_gateway_deployment.deployment.invoke_url}/dev/api/github/webhook"
-  description = "Use this webhook URL in your GitHub App settings"
-}
