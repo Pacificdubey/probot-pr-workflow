df77c2c5d914805eac14384565e6af575270e78a
Prashant dubey <pacific.prashantdubey@gmail.com>
2025-07-07 11:38:31 +0530

    chore: Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,23 @@
+variable "lambda_timeout" {
+  description = "Timeout for the Lambda function in seconds"
+  type        = number
+  default     = 360  
+}
+
+variable "app_id" {
+  description = "GitHub App ID"
+  type        = string
+  sensitive = true
+}
+
+variable "webhook_secret" {
+  description = "GitHub webhook secret"
+  type        = string
+  sensitive = true
+}
+
+variable "private_key" {
+  description = "GitHub App private key"
+  type        = string
+  sensitive = true
+}
