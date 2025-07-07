3ab119c5af192908846472bb3c0481e15c911ab1
Prashant dubey <pacific.prashantdubey@gmail.com>
2025-07-07 13:37:13 +0530

    Update api-gateway.tf

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -1,57 +1,32 @@
-resource "aws_api_gateway_rest_api" "probot_api" {
-  name = "probot-api"
+resource "aws_apigatewayv2_api" "http_api" {
+  name          = "probot-http-api"
+  protocol_type = "HTTP"
 }
 
-resource "aws_api_gateway_resource" "api" {
-  rest_api_id = aws_api_gateway_rest_api.probot_api.id
-  parent_id   = aws_api_gateway_rest_api.probot_api.root_resource_id
-  path_part   = "api"
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id                 = aws_apigatewayv2_api.http_api.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.probot.invoke_arn
+  integration_method     = "POST"
+  payload_format_version = "2.0"
 }
 
-resource "aws_api_gateway_resource" "github" {
-  rest_api_id = aws_api_gateway_rest_api.probot_api.id
-  parent_id   = aws_api_gateway_resource.api.id
-  path_part   = "github"
+resource "aws_apigatewayv2_route" "lambda_route" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "POST /api/github/webhooks"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
 }
 
-resource "aws_api_gateway_resource" "webhook" {
-  rest_api_id = aws_api_gateway_rest_api.probot_api.id
-  parent_id   = aws_api_gateway_resource.github.id
-  path_part   = "webhook"
-}
-
-
-resource "aws_api_gateway_method" "webhook_post" {
-  rest_api_id   = aws_api_gateway_rest_api.probot_api.id
-  resource_id   = aws_api_gateway_resource.webhook.id
-  http_method   = "POST"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "lambda_proxy" {
-  rest_api_id             = aws_api_gateway_rest_api.probot_api.id
-  resource_id             = aws_api_gateway_resource.webhook.id
-  http_method             = aws_api_gateway_method.webhook_post.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.probot.invoke_arn
-}
-
-resource "aws_lambda_permission" "allow_apigw" {
-  statement_id  = "AllowExecutionFromAPIGateway"
+resource "aws_lambda_permission" "apigw_invoke" {
+  statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.probot.function_name
+  function_name = aws_lambda_function.probot.arn
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.probot_api.execution_arn}/*/*"
+  source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
 }
 
-resource "aws_api_gateway_deployment" "deployment" {
-  depends_on = [aws_api_gateway_integration.lambda_proxy]
-  rest_api_id = aws_api_gateway_rest_api.probot_api.id
-}
-
-resource "aws_api_gateway_stage" "probot_stage" {
-  rest_api_id    = aws_api_gateway_rest_api.probot_api.id
-  deployment_id  = aws_api_gateway_deployment.deployment.id
-  stage_name     = "dev"
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.http_api.id
+  name        = "$default"
+  auto_deploy = true
 }
